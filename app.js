af457645523635cb691155b2fbbb997bd8734499
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-08-22 17:53:28 +0600

    select form and call 2 function
    select form and call 2 function

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // Write Your Javascript Code Here
+class App {
+  constructor() {
+    this.form = document.querySelector("#form");
+    this.addEventListeners();
+  }
+  addEventListeners() {
+    document.body.addEventListener("click", (event) => {
+      this.handleFormClick(event);
+    });
+  }
+  handleFormClick(event) {
+    const isFormClicked = this.form.contains(event.target);
+    if (isFormClicked) {
+      //expand the form
+      this.openForm();
+    } else {
+      //shrink the form
+      this.closeForm();
+    }
+  }
+}
+new App();
