0aebb513c10a90323078add83c2bee96658a389e
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-08-22 19:47:12 +0600

    New function created addNote({ title, text })
    New function created addNote({ title, text })

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ class App {
   constructor() {
     this.form = document.querySelector("#form");
     this.noteTitle = document.querySelector("#note-title");
+    this.noteText = document.querySelector("#note-text");
     this.formButtons = document.querySelector("#form-buttons");
     //method
     this.addEventListeners();
@@ -10,6 +11,15 @@ class App {
   addEventListeners() {
     document.body.addEventListener("click", (event) => {
       this.handleFormClick(event);
+    });
+    this.form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const title = this.moteTitle.vlaue;
+      const text = this.noteText.vlaue;
+      const hasNote = title || text;
+      if (hasNote) {
+        this.addNote({ title, text });
+      }
     });
   }
   handleFormClick(event) {
